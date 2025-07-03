3accf914273990f50c011a507711b096a0c3fd8f
sushanshetty1 <sushanshetty1470@gmail.com>
2025-07-03 12:43:53 +0530

    fix: update mobile layout styles for improved responsiveness and consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,19 @@ const page = () => {
 			/>
 
 			<div
-				className="md:hidden absolute top-32 right-0 w-2/3 h-3/4 pointer-events-none z-0 bg-[url('/mobile_rocket.png')] bg-contain bg-no-repeat"
+				className="md:hidden fixed top-24 -right-8 w-4/5 h-4/5 pointer-events-none z-0"
 				style={{
+					backgroundImage: "url('/mobile_rocket1.png')",
+					backgroundSize: "contain",
+					backgroundRepeat: "no-repeat",
 					backgroundPosition: "top right",
 				}}
 			/>
 
-			<div className="h-screen flex items-center justify-center md:justify-start relative z-10 select-none">
-				<div className="w-full md:w-3/5 lg:w-2/3 xl:w-3/5 px-6 pr-12 pl-14 sm:px-8 sm:pr-16 sm:pl-10 md:px-12 lg:px-16 text-left">
+			<div className="h-screen flex items-center justify-center md:justify-start relative z-10">
+				<div className="w-full md:w-3/5 lg:w-2/3 xl:w-3/5 px-4 pr-8 pl-8 sm:px-8 sm:pr-16 sm:pl-10 md:px-12 lg:px-16 text-left">
 					<div className="space-y-4 lg:space-y-6">
-						<h1 className="lilita-font text-7xl sm:text-8xl md:text-7xl lg:text-8xl xl:text-9xl font-bold text-flc-yellow leading-tight tracking-wide">
+						<h1 className="lilita-font text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold text-flc-yellow leading-tight tracking-wide">
 							<span className="md:hidden">
 								Finite
 								<br />
@@ -35,7 +38,7 @@ const page = () => {
 					</div>
 
 					<div className="mt-4 sm:mt-6 lg:mt-8">
-						<p className="comic-font text-lg sm:text-xl md:text-2xl lg:text-3xl leading-relaxed font-medium max-w-2xl">
+						<p className="comic-font text-base sm:text-lg md:text-2xl lg:text-3xl leading-relaxed font-medium max-w-2xl">
 							<span className="md:hidden">
 								NMAMIT's premier
 								<br />
