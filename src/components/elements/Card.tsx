b02c1252b73b1c87c4f56d096d6e95e0e2ab578f
Subtilizer28 <mathiasashton07@gmail.com>
2025-07-07 15:05:46 +0530

    style: adjust layout and styling in page and Card components

--- src/components/elements/Card.tsx
+++ src/components/elements/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 export default function Card({ image, children }: CardProps) {
 	return (
-		<div className="h-[430px] w-full md:min-w-[22rem] mx-auto rounded-3xl p-1.5 shadow-[0_15px_30px_-5px_rgba(151,65,252,0.2)] bg-[linear-gradient(144deg,#FBCFF4,#E4CCF8,#C4E2F7,#FEF9FF)] dark:bg-[linear-gradient(144deg,#7F439D,#33107C,#060329)] transition-transform hover:-translate-y-1 duration-200">
+		<div className="h-[430px] min-w-[20rem] md:min-w-[22rem] mx-auto rounded-3xl p-1.5 shadow-[0_15px_30px_-5px_rgba(151,65,252,0.2)] bg-[linear-gradient(144deg,#FBCFF4,#E4CCF8,#C4E2F7,#FEF9FF)] dark:bg-[linear-gradient(144deg,#7F439D,#33107C,#060329)] transition-transform hover:-translate-y-1 duration-200">
 			<div className="h-full w-full rounded-2xl bg-white/35 dark:bg-white/15 flex flex-col items-center overflow-hidden">
 				{image && (
 					<Image
@@ -16,7 +16,7 @@ export default function Card({ image, children }: CardProps) {
 						height={330}
 						src={image}
 						alt="Card"
-						className="mb-2 rounded-t-2xl h-[330px] w-full object-cover"
+						className="mb-2 rounded-2xl h-[330px] w-full object-cover p-1"
 					/>
 				)}
 				<div className="w-full flex-1 flex flex-col justify-between">
