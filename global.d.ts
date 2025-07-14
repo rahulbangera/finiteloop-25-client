11d2726b97acd346c65eff41397a57e4a1f53b80
Rahul N Bangera <bn2345890@gmail.com>
2025-07-15 01:47:14 +0530

    feat: payment integration

---
+++ global.d.ts
@@ -0,0 +1,33 @@
+interface RazorPayOptions {
+	key: string;
+	amount: number;
+	currency: string;
+	name: string;
+	description: string;
+	image: string;
+	order_id: string;
+	handler: (response: {
+		razorpay_payment_id: string;
+		razorpay_order_id: string;
+		razorpay_signature: string;
+	}) => Promise<void>;
+	prefill?: {
+		name?: string;
+		email?: string;
+		contact?: string;
+	};
+	notes: {
+		address: string;
+	};
+	theme: {
+		color: string;
+	};
+}
+
+interface RazorPay {
+	open: () => void;
+}
+
+interface Window {
+	Razorpay: new (options: RazorPayOptions) => RazorPay;
+}
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useSession } from "next-auth/react";
+import PaymentButton from "../razorpay/paymentButton";
 
 export default function Profile() {
 	const { data: session } = useSession();
@@ -7,6 +8,21 @@ export default function Profile() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen text-6xl font-bold">
 			Hello {session?.user?.name || "Guest"}!
+			<PaymentButton
+				className="flex cursor-pointer"
+				paymentType="MEMBERSHIP"
+				description="Club Membership"
+				onSuccess={async (paymentId) => {
+					// toast.success("Payment successful");
+					alert("Payment successful");
+					console.log("Payment ID:", paymentId);
+				}}
+				onFailure={() => {
+					alert("Payment failed");
+					// toast.error("Payment failed")
+				}}
+				type="submit"
+			/>
 		</div>
 	);
 }
